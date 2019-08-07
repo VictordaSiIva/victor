8a981bb4d63536735c45089b3c92c5a39767d779
VictordaSiIva <victor10019@hotmail.com>
2019-08-07 19:28:34 -0300

    tela1 finalizado

---
+++ BonApptit/BonApptit/Views/NomeUsuario.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BonApptit.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NomeUsuario : ContentPage
+	{
+		public NomeUsuario ()
+		{
+			InitializeComponent ();
+		}
+
+
+    }
+}
---
+++ BonApptit/BonApptit/Views/TelaInicial.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace BonApptit.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TelaInicial : ContentPage
+	{
+		public TelaInicial ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoNomeUsuario(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new BonApptit.Views.NomeUsuario();
+        }
+
+    }
+}
